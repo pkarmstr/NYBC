b5dd056f6707b011acc5ed3884007f0ece05053f
keelan <pkarmstr@gmail.com>
2014-05-05 17:19:18 -0400

    added yiddish tokenizer

---
+++ YiddishUtils/src/org/bikher/yiddish/YiddishCharFilter.java
@@ -0,0 +1,19 @@
+package org.bikher.yiddish;
+
+public class YiddishCharFilter {
+	
+	static String diacriticsPattern = "[\u05B0\u05B1\u05B2\u05B3\u05B4\u05B5" +
+			"\u05B6\u05B7\u05B8\u05B9\u05BC\u05BB\u05BC\u05BD\u05BF\u05B0\u05C1" +
+			"\u05C2\u05C4]";
+	
+	public static String removeDiacritics(String input) {
+		return input.replaceAll(diacriticsPattern, "");
+	}
+	
+	public static void main(String[] args) {
+		String s = "רבּי";
+		System.out.println(s);
+		s = removeDiacritics(s);
+		System.out.println(s);
+	}
+}
---
+++ YiddishUtils/src/org/bikher/yiddish/YiddishTokenizer.java
@@ -0,0 +1,108 @@
+package org.bikher.yiddish;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class YiddishTokenizer {
+	
+	public static List<List<String>> tokenize(String input) {
+		input = YiddishCharFilter.removeDiacritics(input);
+		String[] sentences = sentenceTokenize(input);
+		return wordTokenize(sentences);
+	}
+	
+	public static String[] sentenceTokenize(String input) {
+		input = input.replaceAll("([\\.!?])", "$1 ||||");
+		return input.split("\\|\\|\\|\\|");
+	}
+
+	public static List<List<String>> wordTokenize(String[] sentences) {
+		List<List<String>> fullyTokenized = new ArrayList<List<String>>();
+		for (String s : sentences) {
+			List<String> words = Arrays.asList(s.split(" "));
+			List<String> splitted = splitWords(words);
+			fullyTokenized.add(splitted);
+		}
+		return fullyTokenized;
+	}
+	
+	public static List<String> splitWords(List<String> words) {
+		List<String> splitted = new ArrayList<String>();
+		
+		for (String w : words) {
+			String[] expanded = expandContraction(w);
+			for (int i = 0; i < expanded.length-1; i++) {
+				splitted.add(expanded[i]);
+			}
+			expanded = expandPunctuation(expanded[expanded.length-1]);
+			splitted.addAll(Arrays.asList(expanded));
+		}
+		
+		return splitted;
+	}
+	
+	public static String[] expandPunctuation(String word) {
+		if (word.length() < 2) {
+			return new String[]{word};
+		}
+		
+		if (word.charAt(0) == '„' || word.charAt(0) == '\"') {
+			word = word.substring(0, 1) + " " + word.substring(1);
+		}
+		
+		
+		if (word.indexOf("\"") != -1) {
+			word = word.replace("\"", " \"");
+		}
+		
+		if (word.indexOf(",") != -1) {
+			word = word.replace(",", " ,");
+		}
+		
+		if (word.indexOf(":") != -1) {
+			word = word.replace(":", " :");
+		}
+		
+		if (word.indexOf("-") != -1) {
+			word = word.replace("-", " - ");
+		}
+		
+		return word.split(" ");
+	}
+	
+	public static String[] expandContraction(String word) {
+		if (word.indexOf("'") != -1) {
+			if (word.matches("\\w{1,2}'\\w+")) {
+				return word.replace("'", "' ").split(" ");
+			}
+			else if (word.matches("\\w+?'\\w{1,2}")) {
+				return word.replace("'", " '").split(" ");
+			}
+			else {
+				return new String[]{word};
+			}
+		} 
+		else {
+			return new String[]{word};
+		}
+	}
+	
+	public static void main(String[] args) {
+		String input = "דער רבּי האט מיך געלערענט," +
+				" אַז איך זאל ניט קלערען און ניט פרעגען . וואס גאטט" +
+				" טהוט פערשטעהט ניט אַ מענש , מן-הסתּם האט אַזוי בּעדארפט" +
+				" צוא זיין איך זאל בּלייבּען אַ יתום . „ד' נתן יד' לקח" +
+				" , יהי שם ד' מבורך- , גאט גיט איינעם עלטערען" +
+				" , נעהמט זייא אוועק , זאל גאט'ס נאמען\"" +
+				" זיין געלויבּט , ווייל ער איז דער שופט צדק . ";
+		
+		List<List<String>> t = tokenize(input);
+		for (List<String> ls : t) {
+			for (String s : ls) {
+				System.out.println(s);
+			}
+			System.out.println();
+		}
+	}
+}
